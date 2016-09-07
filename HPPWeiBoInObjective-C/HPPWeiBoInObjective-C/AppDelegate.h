378c13487deba1bff450e8d1d0f417e65158def1
黄晓展 <huangxiaozhan@huangxiaozhandeMacBook-Pro.local>
2016-09-07 22:28:05 +0800

    创建了一个Weibo项目Objective-C

---
+++ HPPWeiBoInObjective-C/HPPWeiBoInObjective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HPPWeiBoInObjective-C
+//
+//  Created by 黄晓展 on 16/9/7.
+//  Copyright © 2016年 黄晓展. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HPPWeiBoInObjective-C/HPPWeiBoInObjective-C/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HPPWeiBoInObjective-C
+//
+//  Created by 黄晓展 on 16/9/7.
+//  Copyright © 2016年 黄晓展. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
